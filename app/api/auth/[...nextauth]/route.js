82c57c72a8eb6a4e7165a3ee8115aba9debbb949
DripLilYo <driplilyo@gmail.com>
2023-08-30 13:32:14 +0300

    smth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,11 +4,10 @@ import NextAuth from "next-auth"
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-          clientId: NEXT_PUBLIC_NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-          clientSecret: NEXT_PUBLIC_NEXT_PUBLIC_GOOGLE_CLIENT_SECRET
+          clientId: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
-      ],
-      secret: process.env.SECRET
+      ]
 })
 
 export { handler as GET, handler as POST }
